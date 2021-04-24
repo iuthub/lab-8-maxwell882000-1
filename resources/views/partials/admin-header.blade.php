fbadd63c71903721b2be88976cce666a3065e50b
Bob <bob@example.com>
2021-04-24 22:25:40 +0500

    made lab

--- resources/views/partials/admin-header.blade.php
+++ resources/views/partials/admin-header.blade.php
@@ -1,10 +1,10 @@
 a<nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
+
             <a href="#" class="navbar-brand">Laravel Guide</a>
             <ul class="nav navbar-nav">
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="{{route('index')}}">Posts</a></li>
             </ul>
         </div>
     </div>
